abb4133b83092434a358b24dca43e11300c49409
MAASIFAR <maasifar@gmail.com>
2024-10-13 09:43:01 +0530

    small

--- src/container/Download.jsx
+++ src/container/Download.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import "../App.css";
 
 const Download = () => {
+  const handleDownloadClick = () => {
+    window.gtag('event', 'download_click', {
+      event_category: 'Download',
+      event_label: 'PicStream Download', // Customize this label as needed
+      transport_type: 'beacon',
+    });
+  };
+
   return (
     <section className="download-section" id="download">
       <h2 className="download-section-title">How to Download</h2>
@@ -13,6 +21,7 @@ const Download = () => {
             href="https://github.com/LockandOpen/PicStream_setup/releases/download/v1.0/PicStream.zip"
             className="download-button"
             download="picStream-setup"
+            onClick={handleDownloadClick} // Attach the click handler
           >
             click here
           </a>
