6628859c780d888d3e5c0064062489250830c046
MAASIFAR <maasifar@gmail.com>
2024-10-16 00:31:01 +0530

    main download link is chnaged

--- src/container/Hero.jsx
+++ src/container/Hero.jsx
@@ -1,34 +1,43 @@
 import React from 'react';
 import '../App.css';
 import logo from '../images/test-removebg-preview.png';
+import ReactGA from 'react-ga4';
 
 const Hero = () => {
+  const handleDownloadClick = () => {
+    // Track button click event
+    ReactGA.event({
+      category: 'Download',
+      action: 'Download button clicked',
+      label: 'PicStream Setup Download',
+    });
+  };
+
   return (
     <section className="hero-section" id="hero">
       <img src={logo} alt="Picstream Logo" className="hero-section-logo" />
-      
+
       <h1 className="hero-section-title">Welcome to Picstream</h1>
-      
+
       <p className="hero-section-description">
-        Your ultimate solution for managing and downloading pictures effortlessly, for free. 
-        PicStream is a powerful tool designed to simplify the process of downloading images in bulk. 
-        Whether you're a designer, researcher, or content creator, PicStream allows you to quickly 
+        Your ultimate solution for managing and downloading pictures effortlessly, for free.
+        PicStream is a powerful tool designed to simplify the process of downloading images in bulk.
+        Whether you're a designer, researcher, or content creator, PicStream allows you to quickly
         and easily collect related images from the web with just one click.
-        Gone are the days of downloading images one by one. PicStream streamlines the process, saving 
-        you time and effort. With its intuitive interface and robust performance, PicStream ensures that 
-        users can gather high-quality images in bulk, making it the ideal solution for anyone in need 
+        Gone are the days of downloading images one by one. PicStream streamlines the process, saving
+        you time and effort. With its intuitive interface and robust performance, PicStream ensures that
+        users can gather high-quality images in bulk, making it the ideal solution for anyone in need
         of large sets of visuals.
       </p>
-      
+
       <a
-      // href="https://drive.google.com/uc?export=download&id=11ipq5cw696uluk6i9TJlKdEbedlo-f5_"
-      
-      href="https://github.com/LockandOpen/PicStream_setup/releases/download/v1.0/PicStream.zip"
-      className="hero-section-cta"
-      download="picStream-setup"
-    >
-      Download Now
-    </a>
+        href="https://github.com/LockandOpen/PicStream_setup/releases/download/v1.1/PicStream_rc.zip"
+        className="hero-section-cta"
+        download="picStream-setup"
+        onClick={handleDownloadClick} // Track the download button click
+      >
+        Download Now
+      </a>
     </section>
   );
 };
